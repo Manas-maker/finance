ba20af7aa5699cf78712554595a754c68110b5b7
Manas Pradhan <manasmpradhan5@gmail.com>
2022-01-14 23:30:15 +0530

    removed redis, trying david's fix to stay logged in

--- app.py
+++ app.py
@@ -7,13 +7,8 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
-import redis
 
 from helpers import apology, login_required, lookup, usd
-
-# Configuring redis
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-redis = redis.from_url(redis_url)
 
 # Get datetime
 date = datetime.now()
@@ -38,7 +33,6 @@ def after_request(response):
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
